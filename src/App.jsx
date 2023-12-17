3701747df1a92fc8bd73b6c7a2a95c4f7314478a
shuhrat567 <isoilovshuhrat567@gmail.com>
2023-12-17 19:43:34 +0300

    card component code commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Card from './components/card/card'
+import { getData } from './constants/db';
+
+
+const courses = getData();
+
+const App = () => {
+  return (
+    <>
+      <h1 className='heading'>Sammi kurslar</h1>
+      {/* Cart */}
+      <div className="cards__container">
+        {courses.map((course) => (
+            <Card key={course.id} course={course}/>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
